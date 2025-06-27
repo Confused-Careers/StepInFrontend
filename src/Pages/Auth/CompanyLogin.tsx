df6303693b9eb3127d16fbb76c78882c54ab70d2
KhushPatel2026 <khushspatel@gmail.com>
2025-06-27 18:18:55 +0530

    Builder Error

--- src/Pages/Auth/CompanyLogin.tsx
+++ src/Pages/Auth/CompanyLogin.tsx
@@ -4,7 +4,7 @@ import StarryBackground from "@/components/Others/StarryBackground";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import authServices from "@/services/authServices";
 import Logo from "../../assets/StepIn Transparent Logo.png";
 import { toast } from "sonner";
