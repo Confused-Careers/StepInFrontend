98fd5e355395b83f78322ad40d3d47e6e3dfefde
NishantMehta07 <nishant07mehta@gmail.com>
2025-07-17 03:39:30 +0530

    route

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { ThemeProvider } from './Contexts/ThemeContext';
+import { ProfileProvider } from './Contexts/ProfileContext';
 import LandingPage from './Pages/Landing/Landing';
 import { OnboardingFlow } from './Pages/Onboarding/OnboardingFlow';
 import IndividualLogin from './Pages/Auth/IndividualLogin';
@@ -27,41 +28,46 @@ import CompanyMessagesPage from './Pages/Company/Company Messages/CompanyMessage
 import JobSeekerMessagesPage from './Pages/JobSeekerMessages/JobSeekerMessagesPage';
 import ApplicantDetailsPage from './Pages/Company/JobPost/ApplicantDetailsPage';
 import Checkout from './components/Popup/checkout';
+import { JobPostingPage } from './Pages/Company/JobPost/PublicJobPost';
+
 
 function App() {
   return (
-    <ThemeProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/individual-login" element={<IndividualLogin />} />
-          <Route path="/individual-register" element={<IndividualRegister />} />
-          <Route path="/individual-forget-password" element={<IndividualResetPassword />} />
-          <Route path="/company/login" element={<CompanyLogin />} />
-          <Route path="/company/register" element={<CompanyRegister />} />
-          <Route path="/company/forgot-password" element={<ForgotPassword />} />
-          <Route path="/onboarding/*" element={<OnboardingFlow />} />
-          <Route path="/auth/google/callback" element={<GoogleAuthCallback />} />
-          <Route path="/contact" element={<ContactUsPage />} />
-          <Route path="/dashboard/profile" element={<DashboardShell><ProfilePage /></DashboardShell>} />
-          <Route path="/dashboard/interactive" element={<DashboardShell><DynamicJobMatching /></DashboardShell>} />
-          <Route path="/dashboard/applications" element={<DashboardShell><ApplicationsPage /></DashboardShell>} />
-          <Route path="/dashboard/messages" element={<DashboardShell><JobSeekerMessagesPage /></DashboardShell>} />
-          <Route path="/dashboard/settings" element={<DashboardShell><SettingsPage /></DashboardShell>} />
-          <Route path="/company/dashboard/profile" element={<CompanyDashboardShell><CompanyProfile /></CompanyDashboardShell>} />
-          <Route path="/company/dashboard/jobposts" element={<CompanyDashboardShell><JobsPostPage /></CompanyDashboardShell>} />
-          <Route path="/company/dashboard/job/new" element={<CompanyDashboardShell><JobPostForm /></CompanyDashboardShell>} />
-          <Route path="/company/dashboard/job/:jobId" element={<CompanyDashboardShell><JobPostForm /></CompanyDashboardShell>} />
-          <Route path="/company/dashboard/:jobId/applications" element={<CompanyDashboardShell><CompanyApplicationsPage /></CompanyDashboardShell>} />
-          <Route path="/company/dashboard/availability" element={<CompanyDashboardShell><AvailabilityPage /></CompanyDashboardShell>} />
-          <Route path="/company/dashboard/company-messages" element={<CompanyDashboardShell><CompanyMessagesPage /></CompanyDashboardShell>} />
-          <Route path="/company/dashboard/:jobId/applications/:applicantId" element={<CompanyDashboardShell><ApplicantDetailsPage /></CompanyDashboardShell>} />
-          <Route path="/checkout" element={<Checkout/>} />
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
-        <Toaster />
-      </Router>
-    </ThemeProvider>
+    <ProfileProvider>
+      <ThemeProvider>
+        <Router>
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/individual-login" element={<IndividualLogin />} />
+            <Route path="/individual-register" element={<IndividualRegister />} />
+            <Route path="/individual-forget-password" element={<IndividualResetPassword />} />
+            <Route path="/company/login" element={<CompanyLogin />} />
+            <Route path="/company/register" element={<CompanyRegister />} />
+            <Route path="/company/forgot-password" element={<ForgotPassword />} />
+            <Route path="/onboarding/*" element={<OnboardingFlow />} />
+            <Route path="/auth/google/callback" element={<GoogleAuthCallback />} />
+            <Route path="/contact" element={<ContactUsPage />} />
+            <Route path="/dashboard/profile" element={<DashboardShell><ProfilePage /></DashboardShell>} />
+            <Route path="/dashboard/interactive" element={<DashboardShell><DynamicJobMatching /></DashboardShell>} />
+            <Route path="/dashboard/applications" element={<DashboardShell><ApplicationsPage /></DashboardShell>} />
+            <Route path="/dashboard/messages" element={<DashboardShell><JobSeekerMessagesPage /></DashboardShell>} />
+            <Route path="/dashboard/settings" element={<DashboardShell><SettingsPage /></DashboardShell>} />
+            <Route path="/company/dashboard/profile" element={<CompanyDashboardShell><CompanyProfile /></CompanyDashboardShell>} />
+            <Route path="/company/dashboard/jobposts" element={<CompanyDashboardShell><JobsPostPage /></CompanyDashboardShell>} />
+            <Route path="/company/dashboard/job/new" element={<CompanyDashboardShell><JobPostForm /></CompanyDashboardShell>} />
+            <Route path="/company/dashboard/job/:jobId" element={<CompanyDashboardShell><JobPostForm /></CompanyDashboardShell>} />
+            <Route path="/company/dashboard/:jobId/applications" element={<CompanyDashboardShell><CompanyApplicationsPage /></CompanyDashboardShell>} />
+            <Route path="/company/dashboard/availability" element={<CompanyDashboardShell><AvailabilityPage /></CompanyDashboardShell>} />
+            <Route path="/company/dashboard/company-messages" element={<CompanyDashboardShell><CompanyMessagesPage /></CompanyDashboardShell>} />
+            <Route path="/company/dashboard/:jobId/applications/:applicantId" element={<CompanyDashboardShell><ApplicantDetailsPage /></CompanyDashboardShell>} />
+            <Route path="/job-posting" element={<JobPostingPage />} />
+            <Route path="/checkout" element={<Checkout/>} />
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
+          <Toaster />
+        </Router>
+      </ThemeProvider>
+    </ProfileProvider>
   );
 }
 
